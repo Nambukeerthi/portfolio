c06f7033204603183f172d31cbf73e5d81b2979a
Nambukeerthi <nambu935@gmail.com>
2024-10-21 21:52:33 +0530

    Update portfo.py

--- portfo.py
+++ portfo.py
@@ -59,6 +59,7 @@ st.markdown(
     3. Airbnb Analysis
     
     4. Industrial Copper Modeling
+          The copper industry deals with less complex data related to sales and pricing. However, this data may suffer from issues such as skewness and noisy data, which can affect the accuracy of manual predictions. Dealing with these challenges manually can be time-consuming and may not result in optimal pricing decisions. A machine learning regression model can address these issues by utilizing advanced techniques such as data normalization, feature scaling, and outlier detection, and leveraging algorithms that are robust to skewed and noisy data. 
     
     """
 )
