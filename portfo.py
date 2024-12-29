23a5e3bb42342d9259fc60a3654d0caabad40cd2
Nambukeerthi <nambu935@gmail.com>
2024-12-29 18:56:42 +0530

    Update portfo.py

--- portfo.py
+++ portfo.py
@@ -14,7 +14,7 @@ col1, col2 = st.columns(2)
 with col1:
     #st.write("portfolio")
     img = Image.open("nambuporfolio.jpeg")
-    st.image( img, use_column_width=True,channels="RGB" )
+    st.image( img, use_container_width=True,channels="RGB" )
     
 with col2:
    
