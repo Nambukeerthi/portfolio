7339435e9072ad0dd3807116b2a976773e27bdbe
NAMBU KEERTHI R <nambu935@gmail.com>
2025-02-20 21:03:53 +0530

    Update portfo.py

--- portfo.py
+++ portfo.py
@@ -13,7 +13,7 @@ col1, col2 = st.columns(2)
 
 with col1:
     #st.write("portfolio")
-    img = Image.open("nambuporfolio.png")
+    img = Image.open("logo.png")
     img_resized = img.resize((300, 400))
     st.image( img_resized,  channels="RGB" )
     
